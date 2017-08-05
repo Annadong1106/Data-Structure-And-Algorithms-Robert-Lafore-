1c3c761ed4bb2a18123be4b3d21b1b2b3a14bef0
Annadong1106 <1281953808@qq.com>
2017-08-05 12:07:29 +0800

    Create InsertionSortApp.java

---
+++ Array/SimpleSorting/InsertionSort/InsertionSortApp.java
@@ -0,0 +1,39 @@
+package InsertionSort;
+
+public class InsertionSortApp 
+{
+	public static void main(String[] args)
+	{
+		int maxSize = 100;
+		ArrayIns arr = new ArrayIns(maxSize);		
+
+		//insert 10 items
+		arr.insert(77);
+		arr.insert(99);
+		arr.insert(44);
+		arr.insert(55);
+		arr.insert(22);
+		arr.insert(88);
+		arr.insert(11);
+		arr.insert(00);
+		arr.insert(66);
+		arr.insert(33);
+		
+		arr.display();
+		
+		arr.insertionSort();
+		arr.display();
+	}
+}
+/*
+ * 在JDK1.8中输入结果为：
+ * --------------------------------------
+ * 77  99  44  55  22  88  11  0  66  33  
+   0  11  22  33  44  55  66  77  88  99  
+   --------------------------------------
+ * */
+
+
+
+
+
