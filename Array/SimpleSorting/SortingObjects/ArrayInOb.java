dc200be9e5bf97c668525cf90f0fa7efa4de8068
Annadong1106 <1281953808@qq.com>
2017-08-05 12:23:02 +0800

    Create ArrayInOb.java

---
+++ Array/SimpleSorting/SortingObjects/ArrayInOb.java
@@ -0,0 +1,65 @@
+package SortingObjects;
+
+public class ArrayInOb
+{
+	private Person[] objectArray;
+	private int maxSize;
+	private int nElems;
+	
+	ArrayInOb(int size)
+	{
+		maxSize = size;
+		objectArray = new Person[maxSize];
+		nElems = 0;
+	}
+	
+	public void insert(String lastName,String firstName,int age)
+	{
+		Person p = new Person(lastName,firstName,age);
+		objectArray[nElems] = p;
+		nElems++;
+	}
+	
+	public void objectSort()   //用插入算法进行排序
+	{
+		int in;
+		int out;
+		
+		for(out = 1;out < nElems;out++)
+		{
+			in = out;
+			Person p = objectArray[out];
+			
+			while(in > 0 && objectArray[in-1].getLastName().compareTo(p.getLastName()) > 0)
+			{
+				objectArray[in] = objectArray[in-1];
+				in--;
+			}
+			
+			objectArray[in] = p;
+		}
+	}
+	
+	public void display()
+	{
+		int i;
+		
+		for(i = 0;i < nElems;i++)
+		{
+			objectArray[i].displayPerson();
+		}
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
