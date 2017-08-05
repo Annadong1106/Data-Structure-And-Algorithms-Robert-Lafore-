e902301445bb907dc4826b9134fa97da61c5d852
Annadong1106 <1281953808@qq.com>
2017-08-05 13:59:52 +0800

    Create StackX.java

---
+++ Stacks and Queues/Stacks/Parsing Arithmetic Expressions using Stack/StackX.java
@@ -0,0 +1,63 @@
+/*
+char[]数组栈，用来存放operator
+*/
+
+public class StackX 
+{
+	private int maxSize;
+	private int top;
+	private char[] stackArray;
+	private int nElems;
+	
+	StackX(int size)
+	{
+		maxSize = size;
+		stackArray = new char[maxSize];
+		top = -1;
+		nElems = 0;
+	}
+	
+	public boolean isFull()
+	{
+		if(top == maxSize - 1)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean isEmpty()
+	{
+		if(top == -1)
+			return true;
+		else
+			return false;
+	}
+	
+	public void push(char value)
+	{
+		if(isFull())
+			return;
+		else
+		{
+			top++;
+			stackArray[top] = value;
+			nElems++;
+		}		
+	}
+	
+	public void pop()
+	{
+		if(isEmpty())
+			return;
+		else
+		{
+			top--;
+			nElems--;
+		}
+	}
+	
+	public char peekTop()
+	{
+		return stackArray[top];
+	}
+}
